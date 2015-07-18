62df93c66451f3dc416d379053a159c88b756f7c
dasher <dasher@crucialdivide.com>
2015-07-18 23:26:49 +0200

    Remove the now ignored directory node_modules

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var through = require('through2'),
     gutil = require('gulp-util'),
     path = require('path'),
     merge = require('merge'),
-    jsonic = require('jsonic')
+    jsonic = require('jsonic');
 
 var PluginError = gutil.PluginError;
 
@@ -23,12 +23,60 @@ module.exports = function (opt) {
         var dest = replaceExtension(file.path);
 
         try {
-            data = JSON.stringify(jsonic(str), null, 2);
+            data = jsonic(str);
         } catch (err) {
             return cb(new PluginError('gulp-mongoose-schema', err));
         }
 
-        file.contents = new Buffer(data);
+        var moment = require("moment");
+        var dateFormatter = require("moment-parseformat");
+
+        /*
+        d = moment(s, 'ddd MMM DD H:mm:ss ZZ YYYY')
+        s = 'Mon Jun 02 10:52:59 +0000 2008';
+        */
+
+        var dateFormats = [
+            'ddd MMM DD H:mm:ss ZZ YYYY',
+            'M/d/yyyy',
+            'dddd, MMMM dd, yyyy',
+            'dddd, MMMM dd, yyyy h:mm:ss tt',
+            'MMMM dd',
+            'ddd, dd MMM yyyy HH:mm:ss GMT',
+            'yyyy-MM-ddTHH:mm:ss',
+            'h:mm tt',
+            'h:mm:ss tt',
+            'yyyy-MM-dd HH:mm:ssZ',
+            'MMMM, yyyy'
+        ];
+
+        // We have the data - process into data types
+        require("sugar");
+        var targetSchema = {};
+        for (key in data) {
+            var dataValue = data[key];
+            var dataType = (typeof dataValue);
+            var isArray = Array.isArray(dataValue);
+
+            if (isArray) {
+                targetSchema[key] = "Array";
+            } else {
+                if (dataType == 'object') {
+                    targetSchema[key] = 'Mixed';
+                } else {
+                    var guessedFormat = dateFormatter(dataValue+'') || '';
+                    console.log("[%s:%j] format guess %s", key, dataValue, guessedFormat);
+                    var dateFormatGuess =  moment(dataValue, dateFormats, true);
+                    if (dateFormatGuess.isValid()) {
+                        targetSchema[key] = "Date";
+                    } else {
+                        targetSchema[key] = dataType.camelize();
+                    }
+                }
+            }
+        }
+
+        file.contents = new Buffer(JSON.stringify(targetSchema), null, 2);
 
         file.path = dest;
         cb(null, file);
